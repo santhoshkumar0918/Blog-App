59c301ee8dc55ff12845fbd60e1e653e8ef68cab
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-07-03 21:38:26 +0530

    added

--- Blog-App/src/components/Input.jsx
+++ Blog-App/src/components/Input.jsx
@@ -5,11 +5,15 @@ const Input  = React.forwardRef( function Input({
     label,
     type = 'Text',
     classname = '',
+    // ...props: This collects the remaining props into a props object.
     ...props},
     ref){
     const id = useId()
    return <>
      <div className='w-full'>
+ {/* {label && ( ... )}: This is a conditional rendering expression.
+It checks if the label variable is truthy. If label is truthy (i.e., it exists and is not null, undefined, or false),
+ then the code inside the parentheses will be rendered. If label is falsy, the code inside the parentheses will not be rendered. */}
         {label &&
         (
             <input htmlfor={id}
