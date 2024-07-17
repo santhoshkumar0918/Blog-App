f180e757d61e1511aacb057818718549cbbe1a09
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-07-17 21:27:50 +0530

    added

--- Blog-App/src/components/post-form/PostForm.jsx
+++ Blog-App/src/components/post-form/PostForm.jsx
@@ -6,12 +6,13 @@ import Button from '../Button.jsx'
 import {useSelector} from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import appwriteService from '../../appwrite/config.js'
+import { useForm } from 'react-hook-form'
 
 
 
 function PostForm( {post}){
   
-   const { regsiter,handleSubmit,control,watch,setValue,getValue} = useform(
+   const { regsiter,handleSubmit,control,watch,setValue,getValue} = useForm(
     {
       defaultValues:{
         value: post?.value || "",
@@ -26,7 +27,25 @@ function PostForm( {post}){
    const navigate = useNavigate()
    const userData = useSelector((state) => state.auth.userData )
 
-   const submit = async () => {
+   const submit = async (data) => {
+    if(post){
+      const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]):null
+    }
+
+    if(file){
+      appwriteService.deleteFile(post.featuredImage)
+    }
+
+    const dbPost = await appwriteService.updatePost(post.$id,{
+      ...data,
+      featuredImage : file ? file.$id : undefined
+    })
+
+      if(dbPost){
+        navigate(`/post/${dbPost.$id}`)
+      }
+     
+   
     
    }
 
@@ -44,8 +63,6 @@ function PostForm( {post}){
 
         }
       })
-      
-      
     },[])
   
 
@@ -57,13 +74,54 @@ function PostForm( {post}){
    <div
    className='flex flex-wrap'>
    <Input 
-   label={title}
-   placeholder = 'title'
-   
-   ></Input>
+   label={Title}
+   placeholder = 'Title'
+   className = 'mb-4'
+   {...regsiter("title", {required: true})}></Input>
+   <Input
+  label="Slug :"
+  placeholder="Slug"
+  className="mb-4"
+  {...regsiter("slug", {required: true})}
+  onInput ={(e) => {
+    setValue("slug",slugTransform(e.currentTarget.value),{shouldValidate:true})
+  } }/>
+  <RTE 
+  label="content"
+  name="content"
+  control={control}
+  defaultValue={getValue("content")}
+  /> </div>
+  <div className='1/3 px-2'>
+  <Input
+  className='mb-4'
+  label='featuredImage'
+  type='file'
+  accept='image/png,image/jpg,image/jpeg'
+  {...regsiter("image",{required:!post})}
+  />
+  {
+    post&&(
+      <div className='w-full mb-4 '>
+        <img src={appwriteService.getFilePreview(post.featuredImage)} alt={post.title}/>
+        <Select
+        options={["active","inactive"]}
+        className='mb-4'
+        label='status'
+       {...regsiter(status,{required:true})}
+        ></Select>
+        <Button
+        type='submit'
+        bgColor={post ? bg-green-500 : undefined}
+        className='w-full'
+        >
+          {post ? "update" : "Submit"}
+        </Button>
+      </div>
+    )
+  }
 
-
-   </div>
+  </div>
 
    </form>
   )
